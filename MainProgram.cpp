3d28aa116c65069ba2b3aa0355c4e13843e26254
sudemeskiyurt <sudem.eskiyurt@bahcesehir.edu.tr>
2026-04-14 15:12:18 +0300

    Update MainProgram.cpp

--- MainProgram.cpp
+++ MainProgram.cpp
@@ -145,7 +145,7 @@ public:
     // Example: "Student(Ali, ID: 101, GPA: 3.5)"
     friend ostream& operator<<(ostream& os, const Student& s) {
         // YOUR CODE HERE
-        os << "Student (" <<s.name <<", ID: "<< s.id<< ", GPA: "<< s.gpa <<")";
+        os << "Student(" <<s.name <<", ID: "<< s.id<< ", GPA: "<< s.gpa <<")";
         return os;
     }
 };
